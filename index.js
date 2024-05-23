3e256e03e50d915aab8553b0a5b86504d25deed3
Nikita <nikita.lnwebworks@gmail.com>
2024-05-23 12:47:15 +0530

    add api

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-const jsonServer = require('json-server')
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
 
-const server = jsonServer.create()
+server.use(middlewares);
+server.use(router);
 
-const router = jsonServer.router('./data/login.json')
-const middlewares = jsonServer.defaults()
- 
-server.use(middlewares)
-server.use(router)
-
-server.listen(process.env.PORT || 5000, () => {
-  console.log('JSON Server is running')
-})
+const port = 5000;
+server.listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`);
+});
